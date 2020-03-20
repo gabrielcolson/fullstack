bfa312e6086cac3a523dc9165af7a154f065fde6
Gabriel Colson <gabrielcolson@hotmail.fr>
2020-03-20 20:37:38 +0100

    fix pwa

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -9,11 +9,23 @@ function MyDocument() {
     <html lang="en">
     <Head>
       {/* PWA primary color */}
-      <meta name="theme-color" content={theme.palette.primary.main} />
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
       />
+      <meta charSet='utf-8'/>
+      <meta http-equiv='X-UA-Compatible' content='IE=edge'/>
+      <meta name='viewport'
+            content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'/>
+      <meta name='description' content='Description'/>
+      <meta name='keywords' content='Keywords'/>
+      <title>Next.js PWA Example</title>
+
+      <link rel="manifest" href="/manifest.json"/>
+      <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16'/>
+      <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32'/>
+      <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+      <meta name="theme-color" content={theme.palette.primary.main} />
     </Head>
     <body>
     <Main />
