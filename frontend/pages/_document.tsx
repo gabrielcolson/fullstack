b65e841630dae88f12b8e950be36e62e2227f23d
Gabriel Colson <gabrielcolson@hotmail.fr>
2020-03-24 16:35:44 +0100

    remove google fonts link

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -26,10 +26,6 @@ class CustomDocument extends Document {
     return (
       <html lang="en">
       <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
         <meta charSet='utf-8'/>
         <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'/>
         <meta name='description' content='Full stack demo'/>
