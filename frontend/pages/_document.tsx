c88fa92f9b4a8c343dab26d51e041b245f0f0938
Gabriel Colson <gabrielcolson@hotmail.fr>
2020-03-24 17:01:32 +0100

    optimize font downloading

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -26,6 +26,14 @@ class CustomDocument extends Document {
     return (
       <html lang="en">
       <Head>
+
+        <link rel="dns-prefetch" href="//fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com/"/>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap"
+        />
+
         <meta charSet='utf-8'/>
         <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'/>
         <meta name='description' content='Full stack demo'/>
